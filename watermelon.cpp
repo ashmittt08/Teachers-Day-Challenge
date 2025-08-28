0659a0d017f8669ddc31666de49e7b23f67c939c
Ashmit Saxena <saxenaashmit85@gmail.com>
2025-08-28 21:37:32 +0530

    Added solution for watermelon.coo

---
+++ watermelon.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int  weight ;
+    cin>>weight;
+   if(weight%2==0 and weight>2){
+    cout<<"Yes"<<endl;
+   }
+   else{
+    cout<<"No"<<endl;
+   }
+   return 0;
+}
